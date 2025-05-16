1ee979be72f512e1fc98acb44251aa38ef54dd43
adriangabrielmanoel <159543227+adriangabrielmanoel@users.noreply.github.com>
2025-05-16 11:10:10 -0300

    Add files via upload

---
+++ jogos/teste.py
@@ -0,0 +1,8 @@
+frutas = ['Pera', 'Panana', 'Melancia', 'Manga', 'Maça','Morango']
+fruta_pedida = input('Qual a fruta que deseja?')
+
+if (fruta_pedida in frutas):
+    print ('Sim, temos a fruta')
+else:
+    print('Não temos a fruta')    
+    
